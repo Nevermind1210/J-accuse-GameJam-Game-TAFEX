23290239fe04cbbde747476c016daaf795022a4f
Xavier Arias <xavier_rate@outlook.com>
2021-09-13 10:20:45 +1000

    Finished Product (No Doxygen)

--- Assets/Scripts/GameOver.cs
+++ Assets/Scripts/GameOver.cs
@@ -8,11 +8,24 @@ using UnityEngine.SceneManagement;
 public class GameOver : MonoBehaviour
 {
     [SerializeField] private GameObject warningText;
+    [SerializeField] private GameObject objectiveText;
+    private void Update()
+    {
+        if (Clues.clues == 4)
+        {
+            objectiveText.SetActive(true);
+        }
+    }
+
+    /// <summary>
+    /// This code is by Unity
+    /// </summary>
+    /// <param name="collider">This parameter allows collider2D to be called and grabs its accessors.</param>
     private void OnTriggerEnter2D(Collider2D collider)
     {
         if (collider.gameObject.tag == "Player" && Clues.clues == 4)
         {
-            
+            SceneManager.LoadScene("Win");
         }
         else
         {
@@ -20,6 +33,10 @@ public class GameOver : MonoBehaviour
         }
     }
 
+    /// <summary>
+    /// Just a routine to allow text to appear and disapear
+    /// </summary>
+    /// <returns> No returns just a return when time reaches in real time.</returns>
     IEnumerator WarningTextUI()
     {
         warningText.SetActive(true);
